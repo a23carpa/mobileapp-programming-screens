a9a5fbdb3e87f8e27a49a407900b632a453912c4
Carl <a23carpa@student.his.se>
2024-04-12 09:08:54 +0200

    Creation of EditText that when button is pushed sends the text inside itself to the other Acitivy

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -6,25 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-
+    EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         button = findViewById(R.id.btnMain);
+        edit = findViewById(R.id.edit);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String item = edit.getText().toString();
+                int finalValue = Integer.parseInt(item);
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtra("name", "Carl");
-                intent.putExtra("age", "31");
+                intent.putExtra("value", finalValue);
                 startActivity(intent);
             }
         });
