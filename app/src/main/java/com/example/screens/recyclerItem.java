10c0012c3d0d6b85ce0d5b95ac7bb9455a42a58d
Carl <a23carpa@student.his.se>
2024-04-15 18:00:23 +0200

    Skapandet av objectet som ska visas i recycleView (recyclerItem.java and recycler_item.xml)

---
+++ app/src/main/java/com/example/screens/recyclerItem.java
@@ -0,0 +1,27 @@
+package com.example.screens;
+
+public class recyclerItem {
+    private String name;
+    private String age;
+
+    public recyclerItem(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
