f34dfebfaad0901dc92317478cdfdcd8360a7a91
Carl <a23carpa@student.his.se>
2024-04-17 08:08:00 +0200

    Method setBackground som ändrar färg på recyclerView sakerna.

--- app/src/main/java/com/example/screens/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/screens/RecyclerViewAdapter.java
@@ -1,15 +1,19 @@
 package com.example.screens;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import java.util.Random;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
     private List<recyclerItem> itemList;
@@ -40,12 +44,16 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView namn;
         TextView ålder;
-
+        LinearLayout layout;
         ViewHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
             namn = itemView.findViewById(R.id.nameText);
             ålder = itemView.findViewById(R.id.ageText);
+
+            layout = itemView.findViewById(R.id.recyclerLayout);
+            setBackground(layout);
+
         }
         @Override
         public void onClick(View view) {
@@ -55,4 +63,17 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public interface OnClickListener {
         void onClick(recyclerItem item);
     }
+
+    public void setBackground(LinearLayout layout){
+        // Ändrar färg varje gång, behöver spara på något sätt + går man tillbaka är det ny färg
+        Random r = new Random();
+        int red=r.nextInt(240 - 0 + 1)+0;
+        int green=r.nextInt(240 - 0 + 1)+0;
+        int blue=r.nextInt(240 - 0 + 1)+0;
+
+        GradientDrawable draw = new GradientDrawable();
+        draw.setColor(Color.rgb(red,green,blue));
+        layout.setBackground(draw);
+    }
+
 }
